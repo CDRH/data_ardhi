b07dacbdcc3715225debeeb427498b76aab5093a
TrisCurd <69926455+TrisCurd@users.noreply.github.com>
2022-09-23 12:15:00 -0500

    the function now writes to a csv. The current metadata now has the title and a list of places as well

--- scripts/python/tei_to_csv.py
+++ scripts/python/tei_to_csv.py
@@ -1,37 +1,75 @@
-#ET is used for reading the xml and pandas is used to create the dataframe for csv
-#Using lxml over the normal elementree because it claims to be faster and allows for xpathing
-#Note: ensure you're using the right form of python
+# ET is used for reading the xml and pandas is used to create the dataframe for csv
+from types import MethodType
+from unicodedata import name
 import xml.etree.ElementTree as ET
 import pandas as pd
-
-#right now looking at 1 file
-
-#need to get each file in the folder eventually
-
-file_path = 'source/tei/ardhi.treaty.00001.xml'
-
-xmlparse = ET.parse(file_path)
-root = xmlparse.getroot()
-#print(root.tag, root.attrib)
-
-namespace = {'space' : 'http://www.tei-c.org/ns/1.0'}
-#titles = root.findall(path="d:title", namespaces=namespace)
-#direct path is space:teiHeader/space:fileDesc/space:titleStmt/space:title
-#goal is to make this relative
-titles = root.findall('space:teiHeader/space:fileDesc/space:titleStmt/space:title', namespaces=namespace)
-test = root[0][0][0][0]
-print(f"titles is {test}")
+import os
 
 
-if len(titles) > 0:
-    print("We got some!")
-else:
-    print("couldn't find anything")
+def get_metadata(root: ET.Element) -> list:
+    '''
+    Given the root, gets all the metadata you want and returns it in a list
+    '''
+    data = []
+    file_name = list(root.attrib.values())
+    data.append(file_name[0])
 
-for title in titles:
-    print(title.tag, title.attrib)
+    # Use root.find if there's only 1 value you want
+    title = root.find('.//{*}title[@level="a"]').text
+
+    data.append(title)
+
+    '''
+     method for multiple values:
+     1. find overarching section 
+     2.loop through
+    '''
+    place__section = root.find('.//{*}keywords[@n="places"]')
+    places = []
+
+    # loop through the children in the section
+    for curr_place in place__section:
+        places.append(curr_place.text)
+
+    places_str = '; '.join(places)
+
+    data.append(places_str)
+
+    return data
 
 
-for child in root:
-    print(child.tag, child.attrib)
+def get_filepaths(dir_path: str) -> list:
+    '''
+    Given the directory's path, returns all the filepaths including the directory
+    '''
+    files = os.listdir(dir_path)
+    # .keep doesn't have data in it
+    files.remove('.keep')
 
+    file_paths = []
+    for curr_file in files:
+        curr_file_path = str(f'{dir_path}/{curr_file}')
+        file_paths.append(curr_file_path)
+
+    return file_paths
+
+
+def main():
+    path = 'source/tei'
+    file_paths = get_filepaths(path)
+
+    file_rows = []
+    for curr_file in file_paths:
+        xmlParse = ET.parse(curr_file)
+        root = xmlParse.getroot()
+        meta_data = get_metadata(root)
+        file_rows.append(meta_data)
+
+    # Create the dataframe and write to csv
+    cols = ['File', 'Title names', 'Places']
+    df = pd.DataFrame(file_rows, columns=cols)
+    df.to_csv('output\csv\output.csv', index=False)
+
+
+if __name__ == '__main__':
+    main()
